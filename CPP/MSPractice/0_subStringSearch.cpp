f08f1aecfa6b643a38b261b46a509b7e5cffe269
mdisieno <mdisieno@gmail.com>
2021-01-01 06:40:09 -0800

    new file:   0_inToColumnArray 	new file:   0_inToColumnArray.cpp 	new file:   0_reverseWordNoNumber.cpp 	new file:   0_stringToInt.cpp 	new file:   0_subStringSearch 	new file:   0_subStringSearch.cpp 	new file:   inToColumnArray 	new file:   mostOccuringWord 	new file:   reverseWordNoNumber 	new file:   subStringSearch 	../.vscode/ 	../0_boilerPlate.cpp 	../AlgoPractice/ 	.vscode/ 	../../Python/2_strings.py 	../../Python/MOSH_PythonBeginner/.vscode/

---
+++ CPP/MSPractice/0_subStringSearch.cpp
@@ -0,0 +1,40 @@
+/*************************************************
+Author:Michael Disieno
+Reason:Practicing for Microsoft Job
+
+Implement substring search  - be sure to solve for edge 
+cases and potentially update solution if necessary  
+
+Proposed solution:
+1.Take in the string
+2.Take in the requested subString
+3.See if it is there
+4. See what edge cases might be?
+**************************************************/
+#include <iostream>
+using namespace std;
+
+string solution(string input, string search){
+string strToSearch = input;
+string subString = search;
+int strFound = 99;
+
+	if (strToSearch.find(subString) != 99){
+		cout << "You found " <<"\"" << subString << "\"" << " in ";
+		cout <<"\"" << strToSearch << ".\"" << endl;
+	}else{
+		cout << "String not found." << endl;
+
+	}
+
+
+}
+
+
+int main(){
+	string input = "This is a test string";
+	string search = ".";
+	
+	solution(input, search);
+	return 0;
+}
