e4a177ce051a3f7f91ed457678f9fd4ff5661649
mdisieno <mdisieno@gmail.com>
2021-01-06 08:49:54 -0800

    modified:   MSPractice/mostOccuringWord 	modified:   MSPractice/mostOccuringWord.cpp

--- CPP/MSPractice/mostOccuringWord.cpp
+++ CPP/MSPractice/mostOccuringWord.cpp
@@ -12,14 +12,15 @@ Reason:Practicing for Microsoft Job
 using namespace std;
 
 string solution(string paragraph){
-	//map to house unique words and coun	
 	map<string, int> uniqueWords;
-	string MostFrequent = "";
-	string temp;
-	cout << "Paragraph inserted: " << paragraph << endl << endl;
+	string mostFrequent = "";
+	string word = "";
+	int maxCount = 0;
+
+	//cout << "Paragraph inserted: " << paragraph << endl << endl;
 	//cout << "The Length of the paragraph is: " << paragraph.length() << endl; //CODE - check paragraph insertion
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-		//Remove all punctuation and drop to lowercase
+		//1.Remove all punctuation and drop to lowercase
 	for (int i = 0;i <= paragraph.length();i++){
 		//cout << i << endl; //CODE TRACE - check increment
 		if(ispunct(paragraph[i])){
@@ -34,37 +35,52 @@ string solution(string paragraph){
 	}
 	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 		//find the most used word.
-		//!!!! You are here, pull words into hash map!!!!
-		//find first space
-		//find second space
-		//pull word from inside those spaces.
-		//check if word is in hash map
-		//if not, add word and increment second value
 
+		//2.Pull out words from spaces
 		for (int i = 0;i <= paragraph.length();i++){
-		if(isspace(paragraph[i])){
-			//cout << "you found a space" << endl; //CODE TRACE - check for spaces
+			if(isspace(paragraph[i])){
+				//cout << "you found a space" << endl; //CODE TRACE - check for spaces
+				//cout << word << endl;
+				//iterate through map to check word
+				//if word is not found
+				if (uniqueWords.find(word) == uniqueWords.end()){
+					//!!!!-Not incrementing-!!!!
+					uniqueWords[word]++;
+					cout << word << " at " << uniqueWords[word] << endl << endl;
+					
+					//Set mostFrequent to next
+					
+					//if max count is currently less than the second val in uniqueWord
+					if(maxCount < uniqueWords[word]){
+						//now make the next word maxCount
+						maxCount = uniqueWords[word];
+						mostFrequent = word;
+						//!!!!-How to print out second value-!!!!
+						//cout << mostFrequent << " is " << uniqueWords[word].second;
+					}
+				}
+				word = "";
+			}else{
+				word = word + paragraph[i];
+			}
 		}
-		if(isalpha(paragraph[i])){
-
-		}
-	}
+		cout << word << endl;
 	//cout << paragraph << endl; //CODE TRACE - pulled out punc/
-	//mostFrequent = Word with highest count
-	return MostFrequent;
+	cout << "Most frequent word is: " << mostFrequent << endl;
+	return mostFrequent;
 };
 
 int main(){
-	string paragraph = "this is a test of the giant voice system.";
-	paragraph += " this is only a test. No need to be alarmed, this is just a test"; 
-	paragraph +="paragraph made to test for words that are constantly used over";
+	string paragraph = "This is a test of the giant voice system.";
+	paragraph += " This is only a test. No need to be alarmed, this is just a test "; 
+	paragraph +="paragraph made to test for words that are constantly used over ";
 	paragraph += "and over again.";
 	
 
-	cout << paragraph << endl; 
+	cout << "Paragraph beforehand: " << paragraph << endl; //CODE TRACE - checking input
 	cout << "-------------------------------------" << endl;
 	solution(paragraph);
-	cout << paragraph << endl; //CODE TRACE - check punc removal
+	//cout << paragraph << endl; //CODE TRACE - check punc removal
 	cout << "-------------------------------------" << endl;
 	//cout << "The most frequent word is:  " << solution(paragraph) << endl << endl;
 
