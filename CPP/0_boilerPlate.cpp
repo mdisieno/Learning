25fbcbb822079f4cdcab95c5579056f2fe3501ab
mdisieno <mdisieno@gmail.com>
2021-01-07 11:20:58 -0800

    new file:   0_boilerPlate.cpp

---
+++ CPP/0_boilerPlate.cpp
@@ -0,0 +1,22 @@
+/*************************************************
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ *************************************************/
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+int main(){
+	
+	//Clean end.
+	cout << endl;
+	return 0;
+}
