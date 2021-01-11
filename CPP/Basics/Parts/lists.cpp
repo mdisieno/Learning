6e8be01ddb4fa16170a65a42c6d0cdd23dcb2c0d
mdisieno <mdisieno@gmail.com>
2021-01-11 08:16:18 -0800

    modified:   lists.cpp

--- CPP/Basics/Parts/lists.cpp
+++ CPP/Basics/Parts/lists.cpp
@@ -19,6 +19,9 @@ using namespace std;
 Lists are a grouping of nodes that have ptrs to before
 and after, as well as the info in the middle.
 
+Lists are typically doubling linked. As in they link to
+and from as show in the Node below.
+
 Node:
         _______
 <------ | prev |
@@ -36,13 +39,48 @@ to the new node.
 Con:
 There is no way to go directly to an element, ie. vector[i]
 -Very much like bus topography.... :(
-    
-*/
 
+
+*/
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 int main(){
-	
+	list<int> myList(5,0);
+        list<int>::iterator outputIt;
+        list<int>::iterator insertIt = myList.begin();
+        //With lists, you can't directly assign values like vectors/arrays
+        //This is because of the structure of the list.
+
+        //Bear with me, hair up the but moment. I felt frisky.
+        //List loop to handle print and such.
+        for (int i = 0; i < 2;i++){
+                if (i == 0){
+                        cout << "Swapping a value:" << endl;
+                        list<int>::iterator it = myList.begin();
+                        //VV-Moves iterator up 2 from beginning-^^.
+                        advance(it, 2);
+                        //from there you deref and assign.
+                        *it = 10;
+                        //"Iterating" through a list. We treat lists like vectors with .begin/.end
+                        for(outputIt = myList.begin();outputIt != myList.end();outputIt++){
+                                cout << *outputIt << " ";
+                        }
+                        cout << endl;
+                }else if (i == 1){
+                        cout << "Inserting Now:" << endl;
+                        advance(insertIt, 3);
+                        myList.insert(insertIt, 14);
+                        //"Iterating" through a list. We treat lists like vectors with .begin/.end
+                        for(outputIt = myList.begin();outputIt != myList.end();outputIt++){
+                                cout << *outputIt << " ";
+                        }
+                        cout << endl;
+                }else{
+                        break;
+                }
+        }
+
+
 	//Clean end.
 	cout << endl;
 	return 0;
