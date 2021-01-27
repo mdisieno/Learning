0fedac28a69892369024434f2b5a8074a454a1a6
mdisieno <mdisieno@gmail.com>
2021-01-27 15:56:47 -0800

    new file:   Variables.java

---
+++ Java/GiraffeAcademy_JavaTutorial/Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] args) {
+        String name = "Tony";
+        int age = 40;
+        System.out.println("There once was a man named " + name + ".");
+        System.out.println("he was " + age + " years old. He really liked");
+        System.out.println("the name " + name  + " but he didnt like being");
+        System.out.println(age + " years old. Most of his friends called him");
+        System.out.println("Tom");
+    }
+
+}
