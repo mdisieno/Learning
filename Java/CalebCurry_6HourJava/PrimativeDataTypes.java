a65e99508704fdb47f087700b5ce869500f8e05e
mdisieno <mdisieno@gmail.com>
2021-01-27 13:10:46 -0800

    modified:   PrimativeDataTypes.java

--- Java/CalebCurry_6HourJava/PrimativeDataTypes.java
+++ Java/CalebCurry_6HourJava/PrimativeDataTypes.java
@@ -1,7 +1,9 @@
 public class PrimativeDataTypes {
     public static void main(String[] args) {
-        int x = 1;
-        double y = 2.00;   
+        
+        //Between 32/64 bit, consider precision
+
+
         boolean isMale = true;
         //byte allows store of ascii
         byte b = 'C';
@@ -9,5 +11,18 @@ public class PrimativeDataTypes {
         char c = 'z';
         //used to store numbers
         short s = 32767;
+        //32bit
+        int x = 1;
+        //64bit - Cap L needed to expand range
+        long l = 24444444444L;
+        //fraction/dec
+        //32 bit
+        float f  = 20.5f;
+        //64 bit.]
+        double y = 2.00;   
+
+
+
+        //I am really missing CPP right now lol
     }
 }
