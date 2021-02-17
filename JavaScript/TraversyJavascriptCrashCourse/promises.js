7aa1e7991892ec490b910e5e7ae769fff647c7dc
mdisieno <mdisieno@gmail.com>
2021-02-17 10:31:43 -0800

    modified:   promises.js

--- JavaScript/TraversyJavascriptCrashCourse/promises.js
+++ JavaScript/TraversyJavascriptCrashCourse/promises.js
@@ -1,1 +1,25 @@
 //learning promises within JS
+
+//creates promise, if successful, push resolve, if fail push reject
+let p = new Promise((resolve, reject) => {
+    let a = 1 +1
+    //if this succeeds, push resolve
+    if (a == 2 ){
+        resolve("Success")
+    //if it fails, pass reject message
+    }else{
+        reject("Failed")
+    }
+})
+
+
+
+
+//executing promise
+
+
+p.then((message) =>{ //if it turns out to be successful
+    console.log('This is in the then ' + message)
+}).catch((message)=> { //if it turns out not to be successful
+    console.log('This is in the catch ' + message)
+})
